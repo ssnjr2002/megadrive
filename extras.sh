3d534c6c89f5ba6a9779dd79c54686640f552720
ssnjrthegr8 <38104354+ssnjrthegr8@users.noreply.github.com>
2020-08-21 10:49:17 +0530

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -4,3 +4,18 @@
 # Check your logs on heroku to see if you were sucessfull
 
 echo "NOTICE: extras.sh is runnning"
+
+# Python
+apt-get install -y python \
+                   python-pip \
+                   python3 \
+                   python3-pip
+
+# Install gclone:
+apt-get install -y systemd golang unzip
+mkdir -p /home/.config/rclone/
+curl -s https://raw.githubusercontent.com/oneindex/script/master/gclone.sh | sudo bash
+
+# MEGA.NZ gclone script
+cp /stuff/mega /usr/local/bin/mega
+sudo chmod 777 /usr/local/bin/mega
